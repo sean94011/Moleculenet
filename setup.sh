b940065ae244652e745da8b6d37aee6d1118ee50
Sean Yao <ckyao2@illinois.edu>
2020-06-28 17:16:57 +0800

    setup.sh editted

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 # install miniconda & deepchem-gpu 2.3
 
+echo ' '
 echo 'Installing Miniconda'
+echo ' '
 
 wget -c https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 
@@ -9,22 +11,22 @@ chmod +x Miniconda3-latest-Linux-x86_64.sh
 sudo bash ./Miniconda3-latest-Linux-x86_64.sh -b -f -p /usr/local
 
 echo ' '
-
 echo 'Installing Deepchem'
+echo ' '
 
 sudo conda install -y -c deepchem -c rdkit -c conda-forge -c omnia deepchem-gpu=2.3.0
 
 echo ' '
-
 echo 'Downgrading scikit-learn to version 0.22'
+echo ' '
 
 pip instal scikit-learn==0.22
 
 # install CUDA 9.2
 
 echo ' '
-
 echo 'Installing CUDA 9.2'
+echo ' '
 
 curl -O https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 
@@ -51,16 +53,16 @@ nvidia-smi
 # install pytorch GPU
 
 echo ' '
-
 echo 'Installing Pytorch GPU'
+echo ' '
 
 sudo conda install pytorch torchvision cudatoolkit=9.2 -c pytorch
 
 # install chemprop
 
 echo ' '
-
 echo 'Installing Chemprop'
+echo ' '
 
 git clone https://github.com/chemprop/chemprop.git
 
@@ -69,5 +71,5 @@ cd chemprop
 sudo conda env create -f environment.yml
 
 echo ' '
-
 echo 'Setup Successfully!'
+echo ' '
